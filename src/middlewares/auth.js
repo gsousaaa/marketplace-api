9800bcb05ad45c34f4379372dc2830664a9cf797
gsousaaa <glaucoezequiel94@gmail.com>
2024-03-06 12:03:37 -0300

    Criação do middleware de autenticação para endpoints privados

---
+++ src/middlewares/auth.js
@@ -0,0 +1,37 @@
+const User = require('../models/User')
+
+module.exports = {
+    private: async (req, res, next) => {
+       // Verificando se o token é enviado no body ou na query
+        if(!req.query.token && !req.body.token) {
+            res.json({notAllowerd: true})
+            return
+        }
+
+        // Preenchendo a variavel token
+        let token = ''
+        if(req.body.token) {
+            token = req.body.token
+        }
+
+        if(req.query.token) {
+            token = req.query.token
+        }
+
+        // Verificando se o token é vazio
+        if(token = '') {
+            res.json({notAllowerd: true})
+            return
+        }
+
+        // verificando se o token é válido
+        const user =  User.findOne({token})
+
+        if(!user) {
+            res.json({notAllowerd: true})
+            return
+        }
+
+        next()
+    }
+} 
--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const Auth = require('../middlewares/auth')
+
 const authController = require("../controllers/authController")
 const userController = require("../controllers/userController")
 const adsController = require("../controllers/adsController")
@@ -14,14 +16,14 @@ router.get('/states', userController.getStates)
 router.post('/user/signin', authController.signin)
 router.post('/user/signup', authController.signup)
 
-router.get('/user/me', userController.info)
-router.put('/user/me', userController.editInfo)
+router.get('/user/me', Auth.private, userController.info)
+router.put('/user/me', Auth.private, userController.editInfo)
 
 router.get('/categories', adsController.getCategories)
 
-router.post('/ad/add', adsController.addAction)
+router.post('/ad/add', Auth.private, adsController.addAction)
 router.get('/ad/list', adsController.getList)
 router.get('/ad/item', adsController.getItem) 
-router.post('ad/:id', adsController.editAction)
+router.post('ad/:id', Auth.private, adsController.editAction)
 
 module.exports = router
