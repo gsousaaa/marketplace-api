9b54befe81958065477ccd1e6dd5d01d40f26ea3
gsousaaa <glaucoezequiel94@gmail.com>
2024-03-14 13:08:53 -0300

    Configuração do método addAction, para a criação de anúncios. Please enter the commit message for your changes. Lines starting

--- src/controllers/adsController.js
+++ src/controllers/adsController.js
@@ -1,4 +1,16 @@
+const { v4: uuid } = require('uuid')
+const jimp = require('jimp')
+
 const Category = require('../models/Category')
+const User = require('../models/User')
+const Ad = require('../models/Ad')
+
+const addImage = async (buffer) => {
+    let newName = `${uuid()}.jpg`
+    let tmpImg = await jimp.read(buffer)
+    tmpImg.cover(500, 500).quality(80).write(`./public/media/${newName}`)
+    return newName
+}
 
 module.exports = {
     getCategories: async (req, res) => {
@@ -13,23 +25,81 @@ module.exports = {
             })
         }
 
-        res.json({categories})
+        res.json({ categories })
     },
 
     addAction: async (req, res) => {
+        let { title, price, priceneg, desc, cat, token } = req.body
+        const user = await User.findOne({ token }).exec()
+
+        if (!title || !cat) {
+            res.json({ error: 'Título e/ou categoria não foram preenchidos' })
+            return
+        }
+
+        if (price) {
+            //Tratando o valor de price para inserir no banco de dados
+            price = price.replace('.', '').replace(',', '.').replace('R$ ', '')
+            price = parseFloat(price)
+        } else {
+            price = 0
+        }
+
+        const newAd = new Ad()
+        newAd.idUser = user._id
+        newAd.state = user.state
+        newAd.dateCreated = new Date()
+        newAd.category = cat
+        newAd.title = title
+        newAd.price = price
+        newAd.priceNegotiable = (priceneg == 'true') ? true : false
+        newAd.description = desc
+        newAd.views = 0
+
+        if (req.files && req.files.img) {
+            //verificando se foi enviada apenas uma imagem
+            if (req.files.img.length == undefined) {
+                if (['image/jpeg', 'image/jpg', 'image/png'].includes(req.files.img.mimetype)) {
+                    let url = await addImage(req.files.img.data)
+                    newAd.images.push({
+                        url,
+                        default: false
+                    })
+                } else {
+                    for (let i = 0; i < req.files.img.length; i++) {
+                        if (['image/jpeg', 'image/jpg', 'image/png'].includes(req.files.img[i].mimetype)) {
+                            let url = await addImage(req.files.img[i].data)
+                            newAd.images.push({
+                                url,
+                                default: false
+                            })
+                        }
+                    }
+                }
+            }
+        }
+
+        if (newAd.images.length > 0) {
+            newAd.images[0].default = true
+        }
+
+
+        const info = await newAd.save()
+        res.json({ id: info._id })
+
 
     },
 
     getList: async (req, res) => {
 
-    }, 
+    },
 
     getItem: async (req, res) => {
 
-    }, 
-    
+    },
+
     editAction: async (req, res) => {
 
-    } 
+    }
 }
 
